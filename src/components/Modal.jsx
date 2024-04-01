a9202789683e5ef77585e31d501d066bed58946f
John O'Brien <johnobrien.dev@gmail.com>
2024-04-01 11:32:16 -0400

    add links to projects in modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,9 +5,21 @@ import { FaXmark } from "react-icons/fa6";
 function Modal(props) {
     const {onClose} = props;
     const projects = [
-        {name: 'Kanban', link: '', desc: 'dfgsdfgsdfgsdfg', github: ''}, 
-        {name: 'MemoryCard', link: '', desc: 'dsfgsdfgsdfgsdfgsdfgsdfgdsf', github: ''},
-        {name: 'DiscGolf Scorecard', link: '', desc: 'dsfgsdfgsdfgsdfgsdfgsdfgsdf', github: ''}
+        {name: 'Kanban',
+         link: 'https://johns-kanban.netlify.app/',
+         desc: 'dfgsdfgsdfgsdfg',
+         github: 'https://github.com/johnobriendev/kanbanReact'
+        }, 
+        {name: 'MemoryCard',
+         link: 'https://johnsmemorycard.netlify.app/',
+         desc: 'dsfgsdfgsdfgsdfgsdfgsdfgdsf',
+         github: 'https://github.com/johnobriendev/memory-card'
+        },
+        {name: 'DiscGolf Scorecard',
+         link: 'https://venerable-croissant-c44ec9.netlify.app/',
+         desc: 'dsfgsdfgsdfgsdfgsdfgsdfgsdf',
+         github: 'https://github.com/johnobriendev/discgolfScorecard'
+        }
     ]
     return ReactDom.createPortal(
         <div className="absolute top-0 left-0 w-screen h-screen shadow bg-slate-900 overflow-scroll text-white flex flex-col z-50  gap-2 md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:w-[90vw] md:h-[90vh]">
@@ -22,8 +34,8 @@ function Modal(props) {
                             <h1>{project.name}</h1>
                             <p className="text-xs flex-1">{project.desc}</p>
                             <div className="grid text-center grid-cols-2">
-                                <a className="">Live</a>
-                                <a className=""> GitHub</a>
+                                <a href={project.link} className="pointer-cursor">Live</a>
+                                <a href={project.github} className="pointer-cursor"> GitHub</a>
                             </div>
 
                         </div>
