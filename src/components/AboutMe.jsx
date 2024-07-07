5eca868c7fc61a8782f8d2382ad8d85310c8cce7
John O'Brien <johnobrien.dev@gmail.com>
2024-07-07 14:34:37 -0400

    update homepage

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,17 +8,22 @@ function AboutMe() {
                 <p className="">John O'Brien is a software developer from Bethlehem, Pennsylvania. He holds a B.A. in Economics, Philosophy, and Jazz Studies from the University of Pittsburgh.</p>
 
                 <div className="flex flex-col gap-2">
-                <h1 className="text-2xl">About Me</h1>
-                <ul className="ml-4 list-disc list-outside flex flex-col ">
-                    <li>I have been a professional musician for over 10 years!
-                        <a className="text-sky-500" href="http://johnobrienguitar.com" target="_blank" > Link to my Music Website</a>
-                    </li>
-                    <li>I lived in South America for 2 years and was in Buenos Aires when Argentina won the World Cup.</li>
-                    <li>I speak Spanish!</li>
-                    <li>I am an avid disc golfer. I play multiple times a week.</li>
-                </ul>
-               
-            </div>
+                    <h1 className="text-2xl">About Me</h1>
+                    <div className=" flex flex-col gap-5 ">
+                        <p>I have been a professional musician for over 10 years! I still teach and perform. 
+                            <a className="text-sky-500 underline" href="http://johnobrienguitar.com" target="_blank" > Link to my Music Website</a>
+                        </p>
+                        <p>I lived in South America for 2 years and was living in Buenos Aires when Argentina won the World Cup. I love learning languages and different dialects. I even worked as a translator while I was there! </p>
+                    
+                        <p>When I'm not programming I love to play disc golf and drink mate (vamos argentina) ! Many of my projects feature my personal interests.</p>
+                    </div>
+
+                    <p>Email: <a href="mailto:johnobrien.dev@gmail.com" className="underline">johnobrien.dev@gmail.com</a> </p>
+                    <p>Github: <a href="https://github.com/johnobriendev" className="underline">johnobriendev</a> </p>
+                    <p>LinkedIn: <a href="https://www.linkedin.com/in/john-o-brien-a33bb6275/" className="underline">Here</a>   </p>
+                    
+                
+                 </div>
             </div>
             
             
